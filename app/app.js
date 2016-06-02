af53e9deba165ed9df6693af4f1689e753d85ca8
AJ Saclayan <ajsaclayan@gmail.com>
2016-06-01 22:51:36 -0400

    Got routing to work for signIn.html

--- app/app.js
+++ app/app.js
@@ -1,27 +1,31 @@
-var angularRoute = angular.module('myApp', [require('angular-route')]);
-
-var app = angular.module('myApp', ['ngRoute']);
-
-    app.config(['$routeProvider'], function ($routeProvider) {
-        $routeProvider.when
-            ('/signIn', {
-                templateUrl: 'templates/signIn.html',
-                controller: 'signInCtrl'
+angular.module('myApp', ['ngRoute'])
+    
+.config(function($routeProvider) {
+    $routeProvider.when('/',
+        {
+            templateUrl: 'templates/signIn.html',
+            controller: 'AppCtrl',
+            controllerAs: 'app'
         }).otherwise({
-            redirectTo: 'templates/signIn.html'
-        });
+        redirectTo: 'templates/signIn.html'
     });
-app.controller('loginCtrl', function loginController($scope){
-    var verifyUser = {
-        "username": "aj@swag.com",
-        "password": "swag"
-    };
+})
 
-    $scope.submitForm = function(user){
-        if(user.email === verifyUser.username && user.password === verifyUser.password)
-            alert("Yay");
-        else
-            alert("Shit");
-    };
+// .controller('loginCtrl', function loginController($scope){
+//     var verifyUser = {
+//         "username": "aj@swag.com",
+//         "password": "swag"
+//     };
+//
+//     $scope.submitForm = function(user){
+//         if(user.email === verifyUser.username && user.password === verifyUser.password)
+//             alert("Yay");
+//         else
+//             alert("Shit");
+//     };
+// })
+
+.controller('AppCtrl', function(){
+   var self = this;
+    self.message = "The app routing is working";
 });
-
